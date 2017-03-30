faac73f01e77720fad35e50e4dd68c09bba5ac17
Sean Duffy <sean.the.duffy@gmail.com>
2017-03-30 00:05:32 -0400

    [#142087995] Added checkpoints (Counter/triggers not yet working)

---
+++ driVeR v0.2/Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointScript : MonoBehaviour {
+
+	public PlayerScript pScript;
+    public BoxCollider triggerPoint;
+    public float pCheckpointNum;
+
+	private bool hasPassed;
+
+	// Use this for initialization
+	void Start () {
+        pScript = FindObjectOfType<PlayerScript>();
+        triggerPoint = gameObject.GetComponent<BoxCollider>();
+        pCheckpointNum = pScript.checkpointNum;
+
+		hasPassed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && hasPassed == false)
+        {
+			pCheckpointNum = pCheckpointNum + 1;
+			hasPassed = true;
+        }
+    }
+}
---
+++ driVeR v0.2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float checkpointNum;
+	public Text checkpointText;
+
+	// Use this for initialization
+	void Start () {
+        checkpointNum = 0;
+		checkpointText.text = "Checkpoints: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkpointText.text = "Checkpoints: " + checkpointNum;
+	    
+	}
+}
