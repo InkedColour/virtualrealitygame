a943059beea7e750dda43ce3c52108b8f47b478d
Joseph <j.gcorvetteboy@gmail.com>
2017-04-07 16:34:08 -0400

    Added filter that makes the player's vision look like its underwater

---
+++ driVeR v0.3/Assets/Scripts/WaterFilter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFilter : MonoBehaviour {
+
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y <= -5)
+        {
+            rend.enabled = true;
+        }
+        else
+        {
+            rend.enabled = false;
+        }
+    }
+}
